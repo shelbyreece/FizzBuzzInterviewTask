9af549e3a4e0b2eb7999f80881531a3819ebccd7
shelbyreece <65302587+shelbyreece@users.noreply.github.com>
2026-04-30 10:09:00 -0500

    Add Extended Task Tests to FizzBuzzifyAnIntTests.cs

--- FizzBuzzLib.Tests/FizzBuzzifyAnIntTests.cs
+++ FizzBuzzLib.Tests/FizzBuzzifyAnIntTests.cs
@@ -6,9 +6,32 @@ namespace FizzBuzzLib.Tests
     public class FizzBuzzifyAnIntTests
     {
         [Fact]
-        public void FizzBuzzifyAnInt_WhenPassedAndInt_ReturnsAString()
+        public void FizzBuzzifyAnInt_DefaultRange1To10()
         {
-            // test here
+            var fizzBuzz = new FizzBuzz();
+            var output = fizzBuzz.FizzBuzzValuesForRange(1, 10);
+            output.Should().Be(new[] {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz"});
+        }
+
+        [Fact]
+        public void FizzBuzzifyAnInt_2EvenResults()
+        {
+            var fizzBuzz = new FizzBuzz();
+            fizzBuzz.ReplacementPairs.Clear();
+            fizzBuzz.ReplacementPairs.Add(2, "Even");
+            var output = fizzBuzz.FizzBuzzValuesForRange(1, 10);
+            output.Should().Be(new[] {"1", "Even", "3", "Even", "5", "Even", "7", "Even", "9", "Even"});     
+        }
+
+        [Fact]
+        public void FizzBuzzifyAnInt_2Quack5HonkResults()
+        {
+            var fizzBuzz = new FizzBuzz();
+            fizzBuzz.ReplacementPairs.Clear();
+            fizzBuzz.ReplacementPairs.Add(2, "Quack");
+            fizzBuzz.ReplacementPairs.Add(5, "Honk");
+            var output = fizzBuzz.FizzBuzzValuesForRange(1, 10);
+            output.Should().Be(new[] {"1", "Quack", "3", "Quack", "Honk", "Quack", "7", "Quack", "9", "QuackHonk"});
         }
     }
 }
