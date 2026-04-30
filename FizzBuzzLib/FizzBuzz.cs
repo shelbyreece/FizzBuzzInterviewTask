25aeb3639d7fbbbc2d7933818260e963c91caae4
shelbyreece <65302587+shelbyreece@users.noreply.github.com>
2026-04-30 09:54:52 -0500

    Added Extended Task Logic to FizzBuzz.cs

--- FizzBuzzLib/FizzBuzz.cs
+++ FizzBuzzLib/FizzBuzz.cs
@@ -2,6 +2,14 @@
 {
     public class FizzBuzz : IFizzBuzz, IFizzBuzzRange, IFlexiFizzBuzz
     {
+        public Dictionary<int, string> ReplacementPairs {get;}
+        public FizzBuzz()
+        {
+            ReplacementPairs = new Dictionary<int, string>(){
+                {3, "Fizz"},
+                {5, "Buzz"}
+            };
+        }
         public string FizzBuzzifyAnInt(int input)
         {
             //Step 1 Logic
@@ -22,10 +30,20 @@
                  return input.ToString();
              } */
 
-             //Extended Task Logic
-             
+            //Extended Task Logic
+            string result = "";
+            foreach(var item in ReplacementPairs){
+                if(input % item.Key == 0) {
+                    result += item.Value;
+                }
+            }
+            
+            if(string.IsNullOrEmpty(result)){
+                return input.ToString();
+            }      
+           return result;
         }
-
+            
         public IEnumerable<string> FizzBuzzValuesForRange(int start, int end)
         {
             var results = new List<string>();
