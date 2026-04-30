c2ab22e158a026976a6c0af8dbe040ff22b58baa
shelbyreece <65302587+shelbyreece@users.noreply.github.com>
2026-04-30 08:58:51 -0500

    Implemented Step 1 Logic in FizzBuzz.cs

--- FizzBuzzLib/FizzBuzz.cs
+++ FizzBuzzLib/FizzBuzz.cs
@@ -1,12 +1,25 @@
 ﻿namespace FizzBuzzLib
 {
-    public class FizzBuzz //: IFizzBuzz
+    public class FizzBuzz : IFizzBuzz
     {
-        public string DemoMethod()
+        public string FizzBuzzifyAnInt(int input)
         {
-            // Feel free to delete this method
-
-            return "Hello World";
+              if(input % 3 == 0 && input % 5 == 0)
+              {
+                  return "FizzBuzz";
+              }
+              else if(input % 3 == 0)
+              {
+                  return "Fizz";
+              }
+             else if(input % 5 == 0 )
+             {
+                 return "Buzz";
+             }
+             else
+             {
+                 return input.ToString();
+             }
         }
     }
 }
